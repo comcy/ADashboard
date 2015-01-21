539e764cc0be23f8dd51b0cdfed3b58e98f98dab
Christian Silfang <christian.silfang@online.de>
2015-01-21 19:56:50 +0100

    Projektdateien hinzugefügt

---
+++ src/main/java/de/tency/adashboard/ADashboardController.java
@@ -0,0 +1,53 @@
+package de.tency.adashboard;
+
+import java.io.IOException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+/**
+ * System controller bean
+ * <code>sposys.xhtml</code>
+ */
+@ManagedBean(name = "controller")
+@RequestScoped
+public class ADashboardController {
+
+    /**
+     * default empty constructor
+     */
+    public ADashboardController() {
+    }
+
+    /**
+     * SPOSys Navigation
+     */
+    
+    /**
+     * Link back to login
+     */
+    public String goLogin() {
+        return "login";
+    }
+
+    /**
+     * Link back to home
+     */
+    public String goHome() {
+        return "adashboard";
+    }
+
+    /**
+     * System logout
+     */
+    public String goLogout() throws IOException {
+        ExternalContext ec = FacesContext.getCurrentInstance()
+                .getExternalContext();
+        ec.invalidateSession();
+        ec.redirect("adashboard.jsf");
+
+        return null;
+    }
+
+}
